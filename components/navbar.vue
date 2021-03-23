d99d3b16b9c1d69b883699afc69312990baa6231
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-03-23 16:55:15 +0100

    add navbar, update styling

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="navbar">
+  <div class="comp-navbar">
     <a-menu v-model="current" mode="horizontal">
       <a-menu-item key="dashboard">
         <NuxtLink to="/dashboard"><a-icon type="home" />Dashboard</NuxtLink>
       </a-menu-item>
       <a-menu-item key="chatbot">
-        <NuxtLink to="/chatbot"><a-icon type="message" />chatbot</NuxtLink>
+        <NuxtLink to="/chatbot"><a-icon type="message" />Rapport</NuxtLink>
       </a-menu-item>
       <a-menu-item key="options">
-        <NuxtLink to="/settings"><a-icon type="setting" />Settings</NuxtLink>
+        <NuxtLink to="/settings"><a-icon type="setting" />Options</NuxtLink>
       </a-menu-item>
     </a-menu>
   </div>
@@ -30,9 +30,9 @@ a {
   color: inherit;
 }
 
-.navbar {
-  position: fixed;
-  bottom: 0;
+.comp-navbar {
+  position: block;
+  top: 0;
   width: 100%;
 }
 </style>
