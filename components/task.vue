2cdd7b21a3cfab7b6657694471b0171ac95f2ca7
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-03-23 17:19:34 +0100

    add task component

--- components/task.vue
+++ components/task.vue
@@ -1,10 +1,92 @@
 <template>
-  <div class="comp-task"> 
+  <div class="comp-task" :style="[task.started ? { backgroundColor: 'lightgreen' } : { backgroundColor: '' }]">
+    <template v-if="task">
+      <div class="main-info">
+        <div class="task-name">{{ task.name }}</div>
+        <div class="time-passed">{{ printTime(task.timestamp) }}</div>
+        <div class="more-options">
+          <a-popover trigger="click" placement="leftTop">
+            <a slot="content">Edit <br /></a>
+            <a slot="content" @click="deleteTask">Delete<br /></a>
+            <a slot="content">Archive</a>
+            <a-icon type="more" />
+          </a-popover>
+        </div>
+      </div>
+      <div class="estimation" />
+    </template>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
+import { Task } from '~/models/task.model'
+import { Timestamp } from '~/models/timestamp.model'
+import { tasksService } from '~/services/tasks.service'
+export default Vue.extend({
+  props: {
+    task: Task,
+  },
+  data() {
+    return {
+      time: '',
+    }
+  },
+  watch: {
+    'task.started'(started: boolean) {
+      if (started) tasksService.startTask(this.task)
+      else tasksService.stopTask(this.task)
+    },
+  },
+  methods: {
+    printTime(ts: Timestamp) {
+      let ret = ''
+      ret += ts.hour < 10 ? '0' + ts.hour : ts.hour
+      ret += ':'
+      ret += ts.min < 10 ? '0' + ts.min : ts.min
+      ret += ':'
+      ret += ts.sec < 10 ? '0' + ts.sec : ts.sec
+      return ret
+    },
+    deleteTask() {
+      tasksService.deleteTask(this.task)
+    },
+  },
+})
 </script>
 
 <style>
+.task-name,
+.time-passed,
+.more-options {
+  display: inline;
+}
+
+.comp-task {
+  border: 1px solid black;
+  margin-bottom: 5px;
+}
+
+.task-name {
+  float: left;
+  width: 60%;
+}
+
+.time-passed {
+  position: relative;
+  left: 5%;
+}
+
+.more-options {
+  float: right;
+}
+
+.main-info {
+  padding-top: 5px;
+  padding-left: 5px;
+}
+
+.estimation {
+  height: 12px;
+}
 </style>
--- services/tasks.service.ts
+++ services/tasks.service.ts
@@ -6,6 +6,46 @@ class TasksService {
   public DATASTORED_NAME = 'tasks'
   public CYCLE_MS = 1000
 
+  public toggleStopwatch(task: Task) {
+    const tasks = this.getTasks()
+    if (!task.started) {
+      tasks.forEach((t) => {
+        t.started = t.id === task.id
+      })
+      this.addTasks(tasks)
+    } else {
+      task.started = false
+      this.updateTask(task)
+    }
+  }
+
+  public startTask(task: Task) {
+    task.timestamp.timeout = setInterval(function () {
+      task.timestamp.ms += tasksService.CYCLE_MS
+      if (task.timestamp.ms === 1000) {
+        task.timestamp.sec++
+        task.timestamp.ms = 0
+        tasksService.updateTask(task)
+      }
+      if (task.timestamp.sec === 60) {
+        task.timestamp.min++
+        task.timestamp.sec = 0
+      }
+      if (task.timestamp.min === 60) {
+        task.timestamp.hour++
+        task.timestamp.min = 0
+      }
+      if (task.timestamp.hour === 24) {
+        task.timestamp.day++
+        task.timestamp.hour = 0
+      }
+    }, this.CYCLE_MS)
+  }
+
+  public stopTask(task: Task) {
+    clearInterval(task.timestamp.timeout)
+  }
+
   public getTasks(): Task[] {
     const ret: Task[] = []
     const taskList = localStorageService.getStoredData(tasksService.DATASTORED_NAME)
@@ -49,5 +89,32 @@ class TasksService {
     message.success('Task deleted.')
   }
 
+  // public startTask(task: Task) {
+  //   task.timestamp.timeout = setInterval(this.timerCycle(task.timestamp), this.CYCLE_MS)
+  // }
+  //
+  // private timerCycle(ts: Timestamp) {
+  //   ts.ms += this.CYCLE_MS
+  //   this.convertTime(ts)
+  // }
+  //
+  // private convertTime(ts: Timestamp) {
+  //   if (ts.ms === 1000) {
+  //     ts.sec++
+  //     ts.ms = 0
+  //   }
+  //   if (ts.sec === 60) {
+  //     ts.min++
+  //     ts.sec = 0
+  //   }
+  //   if (ts.min === 60) {
+  //     ts.hour++
+  //     ts.min = 0
+  //   }
+  //   if (ts.hour === 24) {
+  //     ts.day++
+  //     ts.hour = 0
+  //   }
+  // }
 }
 export const tasksService = new TasksService()
