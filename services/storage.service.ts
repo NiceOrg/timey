8392a5a7753bbf4e4f09c4f7fdee44c211b8fead
Guillaume Malek <guivome@gmail.com>
2021-03-26 10:53:40 +0100

    add local storage service

---
+++ services/storage.service.ts
@@ -0,0 +1,25 @@
+let target = {} as any
+if (typeof localStorage !== 'undefined') target = localStorage
+
+class StorageService {
+  constructor() {
+    // this.set('tasks', [{ "id": 1, "name": "Faire de la guitare", "started": false, "seconds": 78 }, { "id": 2, "name": "Faire du sport", "started": false, "seconds": 0 }])
+  }
+
+  get(key: string) {
+    const data = target[key]
+    if (!data) return
+    return (data[0] === '{' || data[0] === '[') ? JSON.parse(data) : data
+  }
+
+  set(key: string, data: any) {
+    target[key] = typeof data === 'object' ? JSON.stringify(data) : data
+    return data
+  }
+
+  has(key: string) {
+    return this.get(key).then((value: any) => !!value)
+  }
+}
+
+export const storageService = new StorageService()
