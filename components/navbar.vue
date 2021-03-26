c4831fce5646b5c3b2a07129e3cc9c0c22eb58d5
Guillaume Malek <guivome@gmail.com>
2021-03-26 10:36:10 +0100

    add navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,38 +1,37 @@
 <template>
-  <div class="navbar">
+  <div class="comp-navbar">
     <a-menu v-model="current" mode="horizontal">
       <a-menu-item key="dashboard">
         <NuxtLink to="/dashboard"><a-icon type="home" />Dashboard</NuxtLink>
       </a-menu-item>
-      <a-menu-item key="chatbot">
-        <NuxtLink to="/chatbot"><a-icon type="message" />chatbot</NuxtLink>
+      <a-menu-item key="report">
+        <NuxtLink to="/report"><a-icon type="message" />Rapport</NuxtLink>
       </a-menu-item>
-      <a-menu-item key="options">
-        <NuxtLink to="/settings"><a-icon type="setting" />Settings</NuxtLink>
+      <a-menu-item key="settings">
+        <NuxtLink to="/settings"><a-icon type="setting" />Options</NuxtLink>
       </a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+const DEFAULT_MENU = 'dashboard'
+export default Vue.extend({
   data() {
     return {
-      current: ['dashboard'],
+      current: [DEFAULT_MENU],
     }
   },
-}
+  created() {
+    this.current = [this.$route.name || DEFAULT_MENU]
+  },
+})
 </script>
 
 <style>
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
-.navbar {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
+.ant-menu-item {
+  text-align: center;
+  width: 33%;
 }
 </style>
