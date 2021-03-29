bef774f5930640df42d269c20827b53f650c64a0
Guillaume Malek <guivome@gmail.com>
2021-03-29 11:04:25 +0200

    update style for authentification

--- layouts/auth-layout.vue
+++ layouts/auth-layout.vue
@@ -4,9 +4,4 @@
   </div>
 </template>
 
-<style>
-body {
-  background: url(/assets/background.jpg) repeat;
-  min-height: 100%;
-}
-</style>
+<style></style>
--- pages/create-account.vue
+++ pages/create-account.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="page-create-account">
     <div class="title">Create account</div>
-    <div class="creation">
+    <div class="form">
       <a-form-model ref="ruleForm" :model="ruleForm" :rules="rules" v-bind="layout">
-        <a-form-model-item has-feedback label="Username" prop="username">
-          <a-input v-model.number="ruleForm.username" />
+        <a-form-model-item has-feedback prop="username">
+          <a-input v-model.number="ruleForm.username" placeholder="Username">
+            <a-icon slot="prefix" type="user" style="color: rgba(0, 0, 0, 0.25)" />
+          </a-input>
         </a-form-model-item>
-        <a-form-model-item has-feedback label="Password" prop="pass">
-          <a-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+        <a-form-model-item has-feedback prop="pass">
+          <a-input v-model="ruleForm.pass" type="password" autocomplete="off" placeholder="Password">
+            <a-icon slot="prefix" type="lock" style="color: rgba(0, 0, 0, 0.25)" />
+          </a-input>
         </a-form-model-item>
-        <a-form-model-item has-feedback label="Confirm" prop="checkPass">
-          <a-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+        <a-form-model-item has-feedback prop="checkPass">
+          <a-input v-model="ruleForm.checkPass" type="password" autocomplete="off" placeholder="Repeat password">
+            <a-icon slot="prefix" type="lock" style="color: rgba(0, 0, 0, 0.25)" />
+          </a-input>
         </a-form-model-item>
       </a-form-model>
-    </div>
-    <div class="submit">
       <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
         <a-button type="primary" @click="submitForm('ruleForm')"> Submit </a-button>
-        <NuxtLink to="/index"><a-button style="margin-left: 10px">Back</a-button></NuxtLink>
+        <NuxtLink to="/"><a-button style="margin-left: 10px">Back</a-button></NuxtLink>
       </a-form-model-item>
     </div>
   </div>
@@ -29,22 +33,31 @@ export default {
   layout: 'auth-layout',
   data() {
     const checkUsername = (rule, value, callback) => {
-      if (!value) return callback(new Error('Please input an username'))
+      if (!value) {
+        return callback(new Error('Please input an username'))
+      }
 
       callback()
     }
     const validatePass = (rule, value, callback) => {
-      if (value === '') callback(new Error('Please input the password'))
-      else {
-        if (this.ruleForm.checkPass !== '') this.$refs.ruleForm.validateField('checkPass')
+      if (value === '') {
+        callback(new Error('Please input the password'))
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
 
         callback()
       }
     }
     const validatePass2 = (rule, value, callback) => {
-      if (value === '') callback(new Error('Please input the password again'))
-      else if (value !== this.ruleForm.pass) callback(new Error("Two inputs don't match!"))
-      else callback()
+      if (value === '') {
+        callback(new Error('Please input the password again'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("Two inputs don't match!"))
+      } else {
+        callback()
+      }
     }
     return {
       ruleForm: {
@@ -71,36 +84,24 @@ export default {
 }
 </script>
 
-<style>
-.creation {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  -moz-transform: translateX(-50%) translateY(-50%);
-  -webkit-transform: translateX(-50%) translateY(-50%);
-  transform: translateX(-50%) translateY(-50%);
+<style scoped>
+.page-create-account {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
 }
 
-.submit {
-  position: absolute;
-  top: 65%;
-  left: 50%;
-  -moz-transform: translateX(-50%) translateY(-50%);
-  -webkit-transform: translateX(-50%) translateY(-50%);
-  transform: translateX(-50%) translateY(-50%);
+.ant-form-item {
+  margin-bottom: 0px;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  position: absolute;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-  top: 0%;
-  left: 50%;
-  -moz-transform: translateX(-50%) translateY(-50%);
-  -webkit-transform: translateX(-50%) translateY(-50%);
-  transform: translateX(-50%) translateY(-50%);
+  font-size: 50px;
+}
+
+.form {
+  margin-top: 30px;
 }
 </style>
