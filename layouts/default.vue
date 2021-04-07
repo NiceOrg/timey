a4e436d7ca0bbc9112fba86036a196ebb3bd1823
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-04-07 09:46:56 +0200

    update style, navbar remains fixed when overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="layout-app">
+  <div class="app">
     <navbar />
     <Nuxt />
   </div>
 </template>
+
+<style>
+.app {
+  display: flex;
+  flex-flow: column;
+  overflow: hidden;
+  height: 100%;
+}
+</style>
