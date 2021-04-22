c1c8dff077045d37480f275caa7b70f302a37164
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-04-22 16:24:28 +0200

    update component task

--- plugins/timer.client.ts
+++ plugins/timer.client.ts
@@ -1,13 +1,9 @@
 import { emit } from 'shuutils'
 
 class TimerPlugin {
-
   constructor() {
     const timer = setInterval(() => emit('tick'), 1000)
     window.addEventListener('beforeunload', () => clearInterval(timer))
   }
-
-
-
 }
 export const timerPlugin = new TimerPlugin()
