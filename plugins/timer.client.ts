f741c8ae40a8d3047b0234fed4599e916d260638
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-04-22 15:24:11 +0200

    add timer client

---
+++ plugins/timer.client.ts
@@ -0,0 +1,13 @@
+import { emit } from 'shuutils'
+
+class TimerPlugin {
+
+  constructor() {
+    const timer = setInterval(() => emit('tick'), 1000)
+    window.addEventListener('beforeunload', () => clearInterval(timer))
+  }
+
+
+
+}
+export const timerPlugin = new TimerPlugin()
