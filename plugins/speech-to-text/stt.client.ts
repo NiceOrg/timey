9488d6fe2d561d2a845be4ec21097d810c427dda
Guillaume Malek <guivome@gmail.com>
2021-08-06 11:34:02 +0200

    add STT

---
+++ plugins/speech-to-text/stt.client.ts
@@ -0,0 +1,50 @@
+// https://www.section.io/engineering-educationspeech-recognition-in-javascript/
+// https://stackoverflow.com/questions/38087013/angular2-web-speech-api-voice-recognition
+
+/* eslint-disable no-undef */
+import { emit } from 'shuutils'
+import { SpeechRequest } from '../../models'
+import { STT_RESULT, parametersPlugin } from '../'
+
+class SpeechToTextPlugin {
+  public recognition = {} as SpeechRecognition
+
+  public constructor() {
+    this.load()
+  }
+
+  /* istanbul ignore next */
+  private async load() {
+    const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition
+    const recognition: SpeechRecognition = new SpeechRecognition()
+    recognition.interimResults = false
+    await parametersPlugin.load()
+    recognition.lang = parametersPlugin.getParameters().language
+    recognition.onresult = (event) => this.result(event.results[event.results.length - 1][0].transcript)
+    recognition.onspeechend = () => this.end()
+    this.recognition = recognition
+  }
+
+  /* istanbul ignore next */
+  public result(sentence: string) {
+    const speechRequest = new SpeechRequest({ sentence })
+    emit(STT_RESULT, speechRequest)
+    this.end()
+  }
+
+  /* istanbul ignore next */
+  public start() {
+    this.recognition.start()
+  }
+
+  /* istanbul ignore next */
+  public end() {
+    this.recognition.stop()
+  }
+
+  public execute() {
+    return 'WORK IN PROGRESS'
+  }
+}
+
+export const speechToTextPlugin = new SpeechToTextPlugin()
