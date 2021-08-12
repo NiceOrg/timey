318e87334cc5efa426a262441dfc705ef7b47a45
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-08-12 16:50:19 +0200

    fix intents handling

--- models/chatbot/intents.model.ts
+++ models/chatbot/intents.model.ts
@@ -1,32 +1,25 @@
-/* eslint-disable prettier/prettier */
-export class Intents {
-  add: string[] = ['ajouter', 'ajoute', 'creer', 'crees', 'inserer', 'insere', 'insert']
-  and: string[] = ['et', 'ainsi que', 'ainsi qu']
-  bind: string[] = ['avec', 'qui contient', 'contenant', 'dont', 'ayant comme']
-  color: string[] = ['couleur', 'coloris', 'nuance', 'teinte', 'ton']
-  delete: string[] = ['supprimer', 'supprime', 'enlever', 'enleve']
-  estimation: string[] = ['estimation']
-  identifier: string[] = ['.+?(?=et|avec|couleur| a |$)']
-  language: string[] = ['langue']
-  naming: string[] = ['nom', 'nommee', 'nomme', 'appelle', 'dénomme']
-  pause: string[] = ['pauses', 'pause']
-  resume: string[] = ['reprises', 'reprise']
-  start: string[] = ['lancer', 'lance']
-  stop: string[] = ['arreter', 'arrete', 'stoppe', 'stop']
-  tag: string[] = ['tag', 'etiquette', 'categorie']
-  task: string[] = ['tache']
-  timeSlot: string[] = ['plage horaire']
-  to: string[] = ['a']
-  update: string[] = ['modifier', 'modifie', 'changer', 'change', 'mets', 'mettre']
-  time: string[] = [
-    '.*(semaines|semaine|heures|heure|minutes|minute|secondes|seconde|2[0-3]h[0-5][0-9]|[0-9]h[0-5][0-9]|1[0-9]h[0-5][0-9]|[0-9]h|1[0-9]h|2[0-3]h)'
-  ]
-
-  constructor(data: Partial<Intents> = {}) {
-    Object.assign(this, data)
-  }
-
-  public get(intent: any) {
-    return this[intent]
-  }
+export const INTENTS = {
+  add: ['ajouter', 'ajoute', 'creer', 'crees', 'inserer', 'insere', 'insert'],
+  and: ['et', 'ainsi que', 'ainsi qu'],
+  bind: ['avec', 'qui contient', 'contenant', 'dont', 'ayant comme'],
+  color: ['couleur', 'coloris', 'nuance', 'teinte', 'ton'],
+  delete: ['supprimer', 'supprime', 'enlever', 'enleve'],
+  estimation: ['estimation'],
+  identifier: ['.+?(?=et|avec|couleur| a |$)'],
+  language: ['langue'],
+  naming: ['nom', 'nommee', 'nomme', 'appelle', 'dénomme'],
+  pause: ['pauses', 'pause'],
+  resume: ['reprises', 'reprise'],
+  start: ['lancer', 'lance'],
+  stop: ['arreter', 'arrete', 'stoppe', 'stop'],
+  tag: ['tag', 'etiquette', 'categorie'],
+  task: ['tache'],
+  timeSlot: ['plage horaire'],
+  to: ['a'],
+  update: ['modifier', 'modifie', 'changer', 'change', 'mets', 'mettre'],
+  time: [
+    '.*(semaines|semaine|heures|heure|minutes|minute|secondes|seconde|2[0-3]h[0-5][0-9]|[0-9]h[0-5][0-9]|1[0-9]h[0-5][0-9]|[0-9]h|1[0-9]h|2[0-3]h)',
+  ],
 }
+
+export type IntentName = keyof typeof INTENTS
