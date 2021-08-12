45be6656004adc496dd621d59e9200f50e10ba00
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-08-12 17:41:30 +0200

    add types

--- models/chatbot/intent.model.ts
+++ models/chatbot/intent.model.ts
@@ -1,6 +1,8 @@
+import { IntentName } from './intents.model'
+
 export class Intent {
   word = ''
-  tag = ''
+  tag?: IntentName
 
   constructor(data: Partial<Intent>) {
     Object.assign(this, data)
