b1d5370507ea369f5ee8897dce94a53c19cd26ed
Guillaume Malek <guillaume.malek@outlook.fr>
2021-08-23 14:23:30 +0200

    update api url

--- models/environment.model.ts
+++ models/environment.model.ts
@@ -1,6 +1,6 @@
 export class Environment {
   production = true
-  apiUrl = 'https://timey.ovh'
+  apiUrl = 'https://api.timey.ovh'
 
   constructor(data: Partial<Environment>) {
     Object.assign(this, data)
